71d7452b2419066041bd156e2d649ac779257f17
daveyo89 <j.tothdavid@gmail.com>
2021-08-31 23:56:57 +0200

    clean up docker components

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -3,5 +3,4 @@ import os
 from .utils import get_db_handle
 
 global db, client
-db, client = get_db_handle('mongodb', 'mongodb', 27017, os.getenv('MONGO_INITDB_ROOT_USERNAME'),
-                           os.getenv('MONGO_INITDB_ROOT_PASSWORD'))
+db, client = get_db_handle()
